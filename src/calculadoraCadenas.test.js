a74b3c2af976efe9d4a4dfe1ee6cff2818083786
Focus0Wizard <jhonn.ramirez@ucb.edu.bo>
2026-05-27 17:38:22 -0400

    Segunda Iteracion: La calculadora deberia retornar el mismo numero

--- src/calculadoraCadenas.test.js
+++ src/calculadoraCadenas.test.js
@@ -4,4 +4,9 @@ describe("Calculadora de Cadenas", () => {
   it("debería retornar 0 para una cadena vacía", () => {
     expect(calculadora("")).toEqual(0);
   });
+
+  it("debería retornar el mismo número si la cadena tiene un solo número", () => {
+    expect(calculadora("2")).toEqual(2);
+  });
+
 });
